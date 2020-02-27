3d1904a5915b28d228178004a98ff6318e6a6cec
javaweb <javaweb@info.local>
2020-02-27 20:03:37 -0300

    initial commit

---
+++ src/main/java/br/senai/sp/informatica/associacao/AssociacaoApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sp.informatica.associacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociacaoApplication.class, args);
+	}
+
+}
